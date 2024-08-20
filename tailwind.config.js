81fd715bfd1a66ac946ffaf07b8bae13fa08a33b
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-08-20 22:32:14 +0600

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+    extend: {
+        colors: {
+            clifford: '#da373d',
+            'bike-primary': '#E76F51',
+        }
+    },
+};
+export const plugins = [];
